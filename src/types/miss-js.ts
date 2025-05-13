5464aceaa03c67251cdee0f02d0f38b4defd78d3
F8334479 Plinio Candide Rios Mayer <plinio.mayer@bb.com.br>
2025-05-13 12:58:28 -0300

    fix: imports for miss-js

--- src/types/miss-js.ts
+++ src/types/miss-js.ts
@@ -1,5 +1,5 @@
-import { BooleanIs } from "@boolean";
-import { ObjectIs } from "@object";
+import { BooleanIs } from "@miss-js/boolean";
+import { ObjectIs } from "@miss-js/object";
 
 export type MissJS = {
   is: (value: unknown) => BooleanIs & ObjectIs;
